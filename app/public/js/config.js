a8c2ff579309565a2bd80482524bc881988eafed
harshbaba <harshbaba007@gmail.com>
2019-12-18 19:29:52 +0530

    config update acc to server

--- app/public/js/config.js
+++ app/public/js/config.js
@@ -1,4 +1,4 @@
 var configApp = {
-    //endpoint: "https://poker-play.herokuapp.com",
-    endPoint: "http://localhost:5000",
+    endpoint: "https://poker-play.herokuapp.com",
+    //endPoint: "http://localhost:5000",
 }
